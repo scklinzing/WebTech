5771b595885e3724e6e474e1012dd88ff2d3aba6
Shelley Klinzing <scklinzing@gmail.com>
2014-09-17 09:47:47 -0500

    Used foreach to output $_GET

--- InClassProjects/TestProject3/WebContent/echoform1.php
+++ InClassProjects/TestProject3/WebContent/echoform1.php
@@ -1,6 +1,17 @@
 <?php
 echo "Thank you for responding to URL Nosh";
 echo "<br>";
-print_r($_GET);
+print_r ( $_GET ); // get array
+echo "<br>";
+?>
 
+<?php
+echo "Example 1: All in PHP<br>";
+// $variable are UNTYPED
+// print out values of array
+foreach ( $_GET as $myparam => $myvalue ) {
+	// double quotes evaluate a variable in the line
+	// single quotes is ONLY a string
+	echo "$myparam has value $myvalue<br>";
+}
 ?>
