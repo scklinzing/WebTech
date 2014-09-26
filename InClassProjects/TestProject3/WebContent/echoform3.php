2866f3729b14a542b0b29c2e47eb560b67f77ab6
Shelley Klinzing <scklinzing@gmail.com>
2014-09-26 11:14:36 -0500

    Making changes to SurveyData

---
+++ InClassProjects/TestProject3/WebContent/echoform3.php
@@ -0,0 +1,6 @@
+<h1>Responding to survey</h1>
+<?php
+require("./models/SurveyData.class.php");
+$survey = new SurveyData($_GET);
+$survey->printSurvey();
+?>
--- InClassProjects/TestProject3/WebContent/models/SurveyData.class.php
+++ InClassProjects/TestProject3/WebContent/models/SurveyData.class.php
@@ -1,5 +1,28 @@
 <?php 
 class SurveyData {
+	/* declare variables */
+	private $firstName;
 	
+	/* constructor */
+	public function __constructor($formInput) {
+		$this->initialize($formInput);
+	}
+	
+	/* get and return the firstName */
+	public function getFirstName() {
+		return $this->firstName;
+	}
+	
+	/* print */
+	public function printSurvey() {
+		echo "<h1>URL NOSH Member Survey</h1>";
+		echo "First name: $this->firstName<br>";
+	}
+	
+	/* initialize variables */
+	private function initialize($formInput) {
+		if (isset($formInput['firstname']))
+			$this->firstName = $formInput['firstname'];
+	}
 }
 ?>
