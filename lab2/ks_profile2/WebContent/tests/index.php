026fe6f48e9c36a0df1407b2ecd2855145454d11
Shelley Klinzing <scklinzing@gmail.com>
2014-09-29 09:24:11 -0500

    Completed test_SignUpData.php and updated the corresponding index.php

--- lab2/ks_profile2/WebContent/tests/index.php
+++ lab2/ks_profile2/WebContent/tests/index.php
@@ -1,1 +1,22 @@
-<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Test index page</title>
+<meta name="keywords" content="testpage">
+<meta name="description" content="Test index page">
+</head>
+<body>
+	<h1>Poor man's unit testing</h1>
+	<p>In lab 4 we will introduce unit testing and integration testing. For
+		now we will develop tests that can easily be converted into unit
+		tests.</p>
+	<h3>Functions</h3>
+	<ul>
+		<li><a href="">Empty Test Link</a></li>
+	</ul>
+	<h3>Classes</h3>
+	<ul>
+		<li><a href="test_SignUpData.php">Tests for SignUpData</a></li>
+	</ul>
+</body>
+</html>
