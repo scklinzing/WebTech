485cbe4a5931ab6e6df43391325329624239a6e8
Shelley Klinzing <scklinzing@gmail.com>
2014-10-01 09:34:46 -0500

    Fetched rows to test_DB

--- InClassProjects/TestProject4/WebContent/models/CommentData.class.php
+++ InClassProjects/TestProject4/WebContent/models/CommentData.class.php
@@ -1,26 +1,36 @@
 <?php
+/**
+ * This class does complete input validation.
+ * This will not be a requirement for Lab2.
+ */
 class CommentData {
 	private $firstName;
 	private $evaluationUrl;
 	private $comment;
+	/* constructor */
 	public function __construct($formInput) {
 		$this->initialize ( $formInput );
 	}
+	/* get first name */
 	public function getFirstName() {
 		return $this->firstName;
 	}
+	/* get comment */
 	public function getComment() {
 		return $this->comment;
 	}
+	/* get evaluation url */
 	public function getEvaluationUrl() {
 		return $this->evaluationUrl;
 	}
+	/* print the first name, evaluation url, and comment */
 	public function printComment() {
 		echo "<h1>URL Nosh Member Survey</h1>";
 		echo "First name: $this->firstName<br>";
 		echo "Evaluation url: $this->evaluationUrl<br>";
 		echo "Comment: $this->comment<br>";
 	}
+	/* initialize variables */
 	private function initialize($formInput) {
 		if (isset ( $formInput ['firstName'] ))
 			$this->firstName = $formInput ['firstName'];
--- InClassProjects/TestProject4/WebContent/tests/test_DB.php
+++ InClassProjects/TestProject4/WebContent/tests/test_DB.php
@@ -6,5 +6,17 @@
 	$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 	/* PDO = php data object */
 	$db = new PDO($dsn, $username, $password, $options);
-	echo "Did it";
+	
+	echo "Executing Query<br>";
+	/* select everything from comments */
+	$query = "SELECT * FROM Comments";
+	/* prepare the query */
+	$statement = $db->prepare($query);
+	/* execute the statement */
+	$statement->execute();
+	/* fetch everything from the statement (this will be an array) */
+	$commentRows = $statement->fetchAll();
+	/* print r comment rows */
+	print_r($commentRows);
+	
 ?>
