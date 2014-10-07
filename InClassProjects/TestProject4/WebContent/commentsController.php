ab8500f91a7ca7dab7aaf9ed0a5cf9f0807f30e6
Shelley Klinzing <scklinzing@gmail.com>
2014-10-06 22:16:16 -0700

    Updated commentsController.php

--- InClassProjects/TestProject4/WebContent/commentsController.php
+++ InClassProjects/TestProject4/WebContent/commentsController.php
@@ -2,7 +2,6 @@
 include_once ("views/showComments.php");
 include_once ("models/CommentDB.class.php");
 include_once ("models/CommentData.class.php");
-include_once ("models/CommentData.class.php");
 $myComments = CommentDB::fetchAll ();
 showComments ( $myComments );
 ?>
