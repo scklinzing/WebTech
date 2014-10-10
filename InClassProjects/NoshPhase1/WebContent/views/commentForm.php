daeaa1ee107545407386706fca4026dad2255ab4
Shelley Klinzing <scklinzing@gmail.com>
2014-10-10 09:25:28 -0500

    Added checkbox and radio buttons.

--- InClassProjects/NoshPhase1/WebContent/views/commentForm.php
+++ InClassProjects/NoshPhase1/WebContent/views/commentForm.php
@@ -3,24 +3,41 @@
 <head>
 <title>URL NOSH Comment Form</title>
 <meta name="keywords" content="URL NOSH, Members, URL evaluation">
-<meta name="description" content = "Evaluation form for URLs">
+<meta name="description" content="Evaluation form for URLs">
 </head>
 <body>
-<h1>NOSH URL Survey</h1>
-<p>Give us your candidate comments about a URL worth consideration.</p>
+	<h1>NOSH URL Survey</h1>
+	<p>Give us your candidate comments about a URL worth consideration.</p>
 
-<section>
-<form action ="../controllers/postCommentController.php" method="Post">
-URL for Evaluation:<input type="url" name ="evaluationUrl">
+	<section>
+		<form action="../controllers/postCommentController.php" method="Post">
+			URL for Evaluation:<input type="url" name="evaluationUrl">
 
-<p>
-Give us your critique:<br>
-<textarea name="comment" placeholder="Your comments go here" rows = "20" cols="60">
+			<p>
+				Give us your critique:<br>
+				<textarea name="comment" placeholder="Your comments go here"
+					rows="20" cols="60">
 </textarea>
-</p>
+			</p>
 
-<input type = "submit" name = "submit" value="Submit">
-</form>
-</section>
+			<fieldset>
+				<legend>This comment addresses:</legend>
+				<input type="checkbox" name="commentTagList[]" value="layout">Site
+				layout <input type="checkbox" name="commentTagList[]" value="colors">Color
+				scheme <input type="checkbox" name="commentTagList[]"
+					value="semantics">Semantic organization <input type="checkbox"
+					name="commentTagList[]" value="content">Content
+			</fieldset>
+
+			<p>
+				Nosh member type: <input type="radio" name="memberClassName"
+					value="reader" required>Reader <input type="radio"
+					name="memberClassName" value="nosher">Nosher <input type="radio"
+					name="memberClassName" value="moderator">Moderator
+			</p>
+
+			<input type="submit" name="submit" value="Submit">
+		</form>
+	</section>
 </body>
 </html>
