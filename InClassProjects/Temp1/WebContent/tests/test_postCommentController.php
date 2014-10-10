5071831b8d7cfa3e697f3c51d6cf7a964a51315e
Shelley Klinzing <scklinzing@gmail.com>
2014-10-10 09:12:32 -0500

    Initial commit for Temp1. This may not stay on Github.

---
+++ InClassProjects/Temp1/WebContent/tests/test_postCommentController.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h1>Tests for postCommentController </h1><hr>";
+
+echo "<hr><h2>It should add a comment to the database when a valid form is filled out</h2><hr>";
+
+$_POST = array("evaluationUrl" => "http://npr.org",
+		        "comment" => "This is my comment");
+include("../controllers/postcommentController.php")
+
+?>
