9370e714bf50657cdbb86da2398ee63b87d13b02
Shelley Klinzing <scklinzing@gmail.com>
2014-10-16 20:58:21 -0500

    Finished all testing

--- lab2/ks_profile2/WebContent/tests/test_postUserController.php
+++ lab2/ks_profile2/WebContent/tests/test_postUserController.php
@@ -1,5 +1,6 @@
 <?php
 include_once ("../models/UserDB.class.php");
+include_once ("../models/InterestListDB.class.php");
 
 echo "<h1>Tests for postUserController </h1><hr>";
 
@@ -8,13 +9,14 @@ echo "<hr><h2>It should add a user to the database when a valid form is filled o
 $_POST = array (
 		"username" => "LadyBug",
 		"email" => "lady-bug@mail.com",
-		"pass2" => "password", // password
+		"password" => "password", // password
 		"phoneNum" => "8443819620",
 		"website" => "www.facebook.com",
-		"favcolor" => "#000000", // color
+		"favcolor" => "#ff0000", // color
 		"bday" => "1980-11",
 		"whyRatChat" => "2", // reason
 		"ratsOwned" => "0", // ratsOwned
+		"interestList" => array("ratToys")
 );
 print_r($_POST);
 include_once("../controllers/postUserController.php"); //Make sure you understand
