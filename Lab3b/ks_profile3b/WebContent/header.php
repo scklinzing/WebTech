aa582c1bc6bf61deb7145b83719e518c6ca05466
scklinzing <scklinzing@gmail.com>
2014-11-12 07:06:15 -0800

    Removing stuff from ks_profile3b

--- Lab3b/ks_profile3b/WebContent/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-	<img src="image/rat-chat-banner-728x187.gif" alt="Rat Chat Image"
-		width="728" height="187">
-</header>
-<nav>
-	<a href="../index.html">Home</a> | <a href="">Fancy Rat Varieties</a> |
-	<a href="">Housing</a> | <a href="">Food</a> | <a href="">Toys</a> | <a
-		href="">Links</a> | <a href="">My Profile</a>
-</nav>
--- Lab3b/ks_profile3b/WebContent/tests/test_usersController.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include("../controllers/usersController.php")
-
-?>
