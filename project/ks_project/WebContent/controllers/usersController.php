69a0d0eb2b30a3f2e634cd0912a89cc4948df187
scklinzing <scklinzing@gmail.com>
2014-12-11 17:18:00 -0600

    Fixing some errors

--- project/ks_project/WebContent/controllers/usersController.php
+++ project/ks_project/WebContent/controllers/usersController.php
@@ -12,6 +12,7 @@ include_once(dirname(__FILE__)."/../models/Database.class.php");
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 	<meta charset="UTF-8">
 	<title>Member List Page</title>
+	<style>footer {text-align:center;}</style>
 </head>
 
 <body>
@@ -23,5 +24,11 @@ include_once(dirname(__FILE__)."/../models/Database.class.php");
 			showUsers ( $myUsers, "Here are all our current members!", "../views/userProfile.php?username=");
 		echo '</div>';
 	?>
+	<footer>
+		<p>-x-</p>
+		<p>
+			<i>Rat Chat Website created by Shelley Klinzing</i>
+		</p>
+	</footer>
 </body>
 </html>
